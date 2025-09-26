b5ce0fe3eb263cf35e311b75c2dc2903f8c4dad5
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-09-26 12:31:51 +0530

    counter

--- consumer_console.py
+++ consumer_console.py
@@ -1,6 +1,7 @@
 # consumer_console.py
 import json
 from kafka import KafkaConsumer
+from collections import Counter # for counting images per category
 
 KAFKA_BOOTSTRAP = "localhost:9092"
 KAFKA_TOPIC = "nekos-images"
@@ -12,6 +13,8 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode('utf-8')), # JSON serialization
 )
 
+counter = Counter()
+
 print("Listening for events...\n")
 
 for msg in consumer:
@@ -19,4 +22,6 @@ for msg in consumer:
     cat = event["category"]
     url = event["payload"]["url"]
     artist = event["payload"].get("artist_name", "Unknown")
+    counter[cat] += 1
+    print(counter)
     print(f"[{cat}] {url} (artist: {artist})")
